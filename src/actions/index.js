4456c5a59373033c928a632c669d9e1ed22c4f5d
Zohaib <zohaibmanzoor1700@gmail.com>
2022-06-21 19:23:44 -0700

    counter

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+export const increment = num => {
+    return {
+        type: 'INCREMENT',
+        payload: num || 1
+    }
+}
+
+export const decrement = num => {
+    return {
+        type: 'DECREMENT',
+        payload: num || 1
+    }
+}
+
+export const reset = () => {
+    return {
+        type: 'RESET',
+    }
+}
---
+++ src/reducers/counter.js
@@ -0,0 +1,12 @@
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + action.payload
+        case 'DECREMENT':
+            return state - action.payload 
+        case 'RESET':
+            return state = 0 
+        default:
+            return state
+    }
+}
